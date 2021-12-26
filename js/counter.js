14470397e2f9e0b3ca92816738232c73edf300f0
Roman <romantut1988@mail.ru>
2021-12-26 11:43:38 +0300

    the first counter on the plus or minus buttons

--- js/counter.js
+++ js/counter.js
@@ -1,3 +1,22 @@
-console.log('Hello from Counter JS');
+// Находим элементы на странице
+const btnMinus = document.querySelector('[data-action="minus"]');
+const btnPlus = document.querySelector('[data-action="plus"]');
+const counter = document.querySelector('[data-counter]');
 
+// Отслеживаем клик на кнопку btnMinus
+btnMinus.addEventListener('click', function() {
+
+    // Проверяем чтобы счётчик был больше 1
+    if ( parseInt(counter.innerText) >1 ) {
+        // Изменяем текст в чсётчике уменьшая его на 1
+        counter.innerText = --counter.innerText;
+    }
+
+});
+
+// Отслеживаем клик на кнопку btnPlus
+btnPlus.addEventListener('click', function() {
+    // Изменяем текст в чсётчике увеличивая его на 1
+    counter.innerText = ++counter.innerText;
+});
 
