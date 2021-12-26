2e2fac5ea6fb1d442a246cfe33aa92613403fc3e
Roman <romantut1988@mail.ru>
2021-12-26 12:23:43 +0300

    add a wiretap on the whole window

--- js/counter.js
+++ js/counter.js
@@ -1,22 +1,31 @@
-// Находим элементы на странице
-const btnMinus = document.querySelector('[data-action="minus"]');
-const btnPlus = document.querySelector('[data-action="plus"]');
-const counter = document.querySelector('[data-counter]');
+// Добовляем прослушку на всём окне
+window.addEventListener('click', function (event) {
 
-// Отслеживаем клик на кнопку btnMinus
-btnMinus.addEventListener('click', function() {
+    // Объявляем переменную для счетчика
+    let counter;
 
-    // Проверяем чтобы счётчик был больше 1
-    if ( parseInt(counter.innerText) >1 ) {
-        // Изменяем текст в чсётчике уменьшая его на 1
-        counter.innerText = --counter.innerText;
+    // Проверяем клик строго по кнопкам Плюс или Минус
+    if (event.target.dataset.action === 'plus' || event.target.dataset.action === 'minus') {
+        // Находим обвертку счетчика
+        const counterWrapper = event.target.closest('.counter-wrapper');
+        // Находим див с числом счетчика
+        counter = counterWrapper.querySelector('[data-counter]');
+    }
+
+    // Проверяем является ли элемент по которому был совершен клик кнопкой кнопкой Плюс
+    if (event.target.dataset.action === 'plus') {
+        counter.innerText = ++counter.innerText;
+    }
+
+    // Проверяем является ли элемент по которому был совершен клик кнопкой Минус
+    if (event.target.dataset.action === 'minus') {
+
+        const counterWrapper = event.target.closest('.counter-wrapper');
+        const counter = counterWrapper.querySelector('[data-counter]');
+
+        if (parseInt(counter.innerText) > 1) {
+            counter.innerText = --counter.innerText;
+        }
     }
 
 });
-
-// Отслеживаем клик на кнопку btnPlus
-btnPlus.addEventListener('click', function() {
-    // Изменяем текст в чсётчике увеличивая его на 1
-    counter.innerText = ++counter.innerText;
-});
-
