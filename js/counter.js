5abcd7e46a76610e16271d9aa7e991df28370447
Roman <romantut1988@mail.ru>
2022-01-07 23:58:50 +0300

    removing an item from the js cart

--- js/counter.js
+++ js/counter.js
@@ -29,8 +29,11 @@ window.addEventListener('click', function (event) {
         if (parseInt(counter.innerText) > 1) {
             counter.innerText = --counter.innerText;
         }
-    }
 
+        // Проверка на товар который находится в корзине
+        } else if(event.target.closest('.cart-wrapper') && parseInt(counter.innerText)) {
+            event.target.closest('.cart-item').remove();
+        }
 });
 
 window.addEventListener('click', function (event) {
